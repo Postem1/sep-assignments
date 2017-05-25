97fa0026b58e0dc85bb7beeab58925d5e3f9e19a
Steeve Postemus <spostemus.gmail.com>
2017-05-25 00:39:26 -0700

    checkpoint 5 completed

--- 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/linked_list.rb
@@ -6,25 +6,88 @@ class LinkedList
 
   # This method creates a new `Node` using `data`, and inserts it at the end of the list.
   def add_to_tail(node)
+    current = @head
+
+    if (@head == nil) && (@tail == nil)
+      @head = node
+      @tail = node
+    #add eslif for tail
+    else
+      while current.next != nil
+        current = current.next
+      end
+      current.next = node
+      @tail = node
+      @tail.next = nil
+    end
   end
 
   # This method removes the last node in the lists and must keep the rest of the list intact.
   def remove_tail
+    current = @head
+
+    if @head == @tail
+      @head = nil
+      @tail = nil
+    else
+      while (current.next != nil) && (current.next != @tail)
+        current = current.next
+      end
+      @tail = current
+      @tail.next = nil
+    end
   end
 
   # This method prints out a representation of the list.
   def print
+    current = @head
+
+    puts current.value
+
+    while current.next != nil
+      current = current.next
+      puts current.value
+    end
   end
 
   # This method removes `node` from the list and must keep the rest of the list intact.
   def delete(node)
+    current = @head
+
+    if @head == node
+      remove_front
+    elsif @tail == node
+      remove_tail
+    else
+      while current != node
+        previous = current
+        current = current.next
+      end
+      previous.next = current.next
+    end
   end
 
   # This method adds `node` to the front of the list and must set the list's head to `node`.
   def add_to_front(node)
+    current = node
+    old_head = @head
+
+    if @head == nil
+      @head = current
+      @tail = current
+    else
+      @head = current
+      @head.next = old_head
+    end
   end
 
   # This method removes and returns the first node in the Linked List and must set Linked List's head to the second node.
   def remove_front
+    old_head = @head
+    if (@head == nil) || (@head.next == nil) || (@head == @tail)
+      @head = nil
+    else
+      @head = old_head.next
+    end
   end
 end
--- 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
+++ 01-data-structures/05-hashes-part-2/separate_chaining/separate_chaining.rb
@@ -2,32 +2,105 @@ require_relative 'linked_list'
 
 class SeparateChaining
   attr_reader :max_load_factor
+  attr_reader :item_count
 
-  def initialize(size)
+  def initialize(size_of)
     @max_load_factor = 0.7
+    @items = Array.new(size_of)
+    @item_count = 0.0
   end
 
+  # Adding a new object to the hash given a key and a value
   def []=(key, value)
+    if @items[index(key, size)] == nil
+      @items[index(key, size)] = LinkedList.new
+    end
+
+    @items[index(key, size)].add_to_tail(Node.new(key, value))
+    @item_count += 1
+
+    if load_factor >= @max_load_factor
+      resize
+    end
   end
 
+  # Return the value when a key is given
   def [](key)
+    if @items[index(key,size)] != nil
+      current = @items[index(key, size)].head #chooses head node
+      while current != nil
+        if current.key == key
+          return current.value
+        else
+          current = current.next
+        end
+      end
+    end
   end
 
   # Returns a unique, deterministically reproducible index into an array
   # We are hashing based on strings, let's use the ascii value of each string as
   # a starting point.
   def index(key, size)
+    key.sum % size
   end
 
   # Calculate the current load factor
   def load_factor
+    @item_count / size
   end
 
   # Simple method to return the number of items in the hash
   def size
+    @items.length
   end
 
   # Resize the hash
   def resize
+    old_array = @items
+    @items = Array.new(old_array.length * 2)
+    old_array.each do |list|
+      if list != nil
+        current_node = list.head
+        while current_node != nil
+          if @items[index(current_node.key, size)] == nil
+            @items[index(current_node.key, size)] = LinkedList.new
+            @items[index(current_node.key, size)].add_to_front(Node.new(current_node.key, current_node.value))
+            if load_factor >= @max_load_factor
+              resize
+            end
+          else
+            @items[index(current_node.key, size)].add_to_tail(Node.new(current_node.key, current_node.value))
+            if load_factor >= @max_load_factor
+              resize
+            end
+          end
+          current_node = current_node.next
+        end
+      end
+    end
+  end
+
+  def print_hash
+    puts "The Hash's Load factor is: #{load_factor}, the number of items in the hash is: #{item_count}"
+    @items.each do |list|
+      if list != nil
+        current_node = list.head
+        while current_node != nil
+          puts "Key: #{current_node.key}, Value: #{current_node.value}"
+          current_node = current_node.next
+        end
+      end
+    end
   end
 end
+
+star_wars_movies = SeparateChaining.new(6)
+star_wars_movies["Star Wars: The Phantom Menace"] = "Number One"
+star_wars_movies["Star Wars: Attack of the Clones"] = "Number Two"
+star_wars_movies["Star Wars: Revenge of the Sith"] = "Number Three"
+star_wars_movies["Star Wars: A New Hope"] = "Number Four"
+star_wars_movies["Star Wars: The Empire Strikes Back"] = "Number Five"
+star_wars_movies["Star Wars: Return of the Jedi"] = "Number Six"
+
+star_wars_movies.print_hash
